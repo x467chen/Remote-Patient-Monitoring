0a2800e42bcf688476eae6402818b3d89b8f4465
Kate-XuanqiChen <chenxuanqi1993cookie@gmail.com>
2017-11-14 14:36:05 -0500

    Add files via upload

---
+++ Client(Front-End)/app/src/main/test/java/com/ece651group8/uwaterloo/ca/ece_651_group8/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.ece651group8.uwaterloo.ca.ece_651_group8;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
